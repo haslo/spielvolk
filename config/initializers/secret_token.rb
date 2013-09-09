f9e3b460dfdb9ba259c099bd14d5171a3ae9160c
Guido Gloor <haslo@haslo.ch>
2013-09-09 13:01:45 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spielvolk::Application.config.secret_key_base = '6f716ea460a92c1407f9d52f40438fc7cc397042da8c37d7a7b3c7b74704b4c5b6b893ea97d38bac9b880795f18b7d7be29a9d9fd5afe881ee83936e1ef0dc5f'
